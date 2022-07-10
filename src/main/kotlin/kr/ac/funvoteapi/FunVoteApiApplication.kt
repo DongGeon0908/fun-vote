4cbd89e7d7294333692a1f92883075ad042d4b4e
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-07-10 18:36:03 +0900

    init

---
+++ src/main/kotlin/kr/ac/funvoteapi/FunVoteApiApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.funvoteapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunVoteApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<FunVoteApiApplication>(*args)
+}
